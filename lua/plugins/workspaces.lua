1a2c5d1c63c118cd938d4bce324d4c1ef0146025
Nishant Pillai <nishantpillai5@users.noreply.github.com>
2024-04-28 17:02:07 +0200

    neoscopes session

--- lua/plugins/workspaces.lua
+++ lua/plugins/workspaces.lua
@@ -26,19 +26,25 @@ return {
         else
           require("notify")("Scope refreshed: 󱇳 " .. neoscopes.get_current_scope().name)
 
+          local builtin = require("telescope.builtin")
+
           _G.find_files = function()
-            require("telescope.builtin").find_files({
+            builtin.find_files({
+              prompt_prefix = "󱇳 > ",
               search_dirs = neoscopes.get_current_dirs(),
             })
           end
           _G.live_grep = function()
-            require("telescope.builtin").live_grep({
+            builtin.live_grep({
+              prompt_prefix = "󱇳 > ",
               search_dirs = neoscopes.get_current_dirs(),
+
               additional_args = { "--follow" },
             })
           end
           _G.grep_string = function()
-            require("telescope.builtin").grep_string({
+            builtin.grep_string({
+              prompt_prefix = "󱇳 > ",
               search = vim.fn.input("Search > "),
               search_dirs = neoscopes.get_current_dirs(),
               additional_args = { "--follow" },
@@ -68,10 +74,41 @@ return {
   {
     "stevearc/resession.nvim",
     keys = {
-      { "<leader>ws", "<cmd>lua require('resession').save()<cr>", desc = "Workspace.session_save" },
+      { "<leader>ws", desc = "Workspace.session_save" },
+      { "<leader>wl", desc = "Workspace.session_load" },
+      { "<leader>wS", "<cmd>lua require('resession').save()<cr>", desc = "Workspace.manual_session_save" },
+      { "<leader>wL", "<cmd>lua require('resession').load()<cr>", desc = "Workspace.manual_session_load" },
       { "<leader>wd", "<cmd>lua require('resession').delete()<cr>", desc = "Workspace.session_delete" },
-      { "<leader>wl", "<cmd>lua require('resession').load()<cr>", desc = "Workspace.session_load" },
     },
-    opts = {},
+
+    config = function()
+      local recession = require("resession")
+      recession.setup({})
+
+      local workspace_session = function(action)
+        local status, neoscopes = pcall(require, "neoscopes")
+        if status then
+          local current_scope = neoscopes.get_current_scope()
+          if current_scope ~= nil then
+            action(current_scope.name)
+            require("notify")("Session: " .. current_scope.name)
+          else
+            action("workspace")
+            require("notify")("Session: workspace")
+          end
+        else
+          action("workspace")
+          require("notify")("Session: workspace")
+        end
+      end
+
+      vim.keymap.set("n", "<leader>ws", function()
+        workspace_session(recession.save)
+      end)
+
+      vim.keymap.set("n", "<leader>wl", function()
+        workspace_session(recession.load)
+      end)
+    end,
   },
 }
