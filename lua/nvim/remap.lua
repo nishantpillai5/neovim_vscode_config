f473cb15b25ffc86acc1cfd09d1034548e19eada
Nishant Pillai <nishantpillai5@gmail.com>
2024-05-18 19:08:33 +0200

    moved common keymaps

--- lua/nvim/remap.lua
+++ lua/nvim/remap.lua
@@ -1,22 +1,3 @@
--- Editor
-vim.keymap.set("n", "<leader>s", ":w<cr>", { silent = true, desc = "save" })
-vim.keymap.set("n", "<leader>S", ":wq<cr>", { silent = true, desc = "save_quit" })
-vim.keymap.set("n", "<leader>x", ":q<cr>", { silent = true, desc = "quit" })
-vim.keymap.set("n", "<leader>X", ":q!<cr>", { silent = true, desc = "quit_force" })
-
-vim.keymap.set("n", "<leader>j", ":cnext<cr>", { silent = true, desc = "next_quickfix" })
-vim.keymap.set("n", "<leader>k", ":cprev<cr>", { silent = true, desc = "prev_quickfix" })
-
--- vim.keymap.set("n", "<leader>J", ":lnext<cr>", { silent = true, desc = "next_loclist" })
--- vim.keymap.set("n", "<leader>K", ":lprev<cr>", { silent = true, desc = "prev_loclist" })
-
--- Split
-vim.keymap.set("n", "<leader>zv", "<cmd>vs<cr>", { desc = "Visual.vertical_split" })
-vim.keymap.set("n", "<leader>zs", "<cmd>sp<cr>", { desc = "Visual.horizontal_split" })
-
--- Netrw
-vim.keymap.set("n", "<leader>ef", vim.cmd.Ex, { desc = "Explorer.netrw" })
-
 -- Resize vertically with Ctrl-Right and Ctrl-Left
 vim.keymap.set("n", "<C-Right>", ":vertical resize +2<cr>", { silent = true, desc = "vertical_resize_right" })
 vim.keymap.set("n", "<C-Left>", ":vertical resize -2<cr>", { silent = true, desc = "vertical_resize_left" })
--- lua/vsc/remap/git.lua
+++ lua/vsc/remap/git.lua
@@ -4,6 +4,11 @@ local map = require("vsc.vscode_mapper").map
 map("n", "<leader>gs", "workbench.view.scm")
 map("n", "<leader>gl", "git.viewHistory")
 
+-- map("n", "<leader>gb", "gitlens.showQuickCommitFileDetails")
+map("n", "<leader>gB", "gitlens.toggleFileBlame")
+-- Visual
+map("n", "<leader>zgb", "gitlens.toggleLineBlame")
+
 -- Gitsigns
 map("n", "[c", "workbench.action.editor.previousChange")
 map("n", "]c", "workbench.action.editor.nextChange")
