35f16d9433650e2d20297bce58f9fff1a1e4cbb4
Nishant Pillai <nishantpillai5@users.noreply.github.com>
2024-06-26 12:47:58 +0200

    wider grapple window

--- lua/config/grapple.lua
+++ lua/config/grapple.lua
@@ -2,18 +2,21 @@ local M = {}
 
 M.lualine = function()
   local lualineB = require('lualine').get_config().sections.lualine_b or {}
-  table.insert(lualineB, { "grapple" })
+  table.insert(lualineB, { 'grapple' })
 
   require('lualine').setup { sections = { lualine_b = lualineB } }
 end
 
 M.setup = function()
-  require("grapple").setup({
-    scope = "git",
+  require('grapple').setup {
+    scope = 'git',
+    win_opts = {
+      width = 0.8,
+    },
     integrations = {
-      resession = true
-    }
-  })
+      resession = true,
+    },
+  }
 end
 
 M.config = function()
