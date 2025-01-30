3a00e634c557404d4ac4decacb76b091a0789604
Nishant Pillai <nishantpillai5@users.noreply.github.com>
2025-01-30 16:20:40 +0100

    fix windows open explorer

--- lua/common/utils.lua
+++ lua/common/utils.lua
@@ -136,23 +136,13 @@ end
 function M.open_explorer(path)
   local os = require('common.env').OS
   if os == 'mac' then
-    -- macOs: open file in default application in the background.
     vim.fn.jobstart({ 'xdg-open', '-g', path }, { detach = true })
   end
   if os == 'linux' then
-    -- Linux: open file in default application
     vim.fn.jobstart({ 'xdg-open', path }, { detach = true })
   end
   if os == 'windows' then
-    -- Windows: Without removing the file from the path, it opens in code.exe instead of explorer.exe
-    local p
-    local lastSlashIndex = path:match '^.+()\\[^\\]*$' -- Match the last slash and everything before it
-    if lastSlashIndex then
-      p = path:sub(1, lastSlashIndex - 1) -- Extract substring before the last slash
-    else
-      p = path -- If no slash found, return original path
-    end
-    vim.cmd('silent !start explorer ' .. p)
+    vim.cmd('silent !start explorer ' .. path)
   end
 end
 
