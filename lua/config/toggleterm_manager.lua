3645aefb1ae9b9fb8b31cf3b8feaa5f152e5e047
Nishant Pillai <nishantpillai5@users.noreply.github.com>
2025-06-01 01:34:37 +0200

    terminal telescope

--- lua/config/toggleterm_manager.lua
+++ lua/config/toggleterm_manager.lua
@@ -1,8 +1,8 @@
 local M = {}
 
 M.keys = {
-  { '<leader>fO', desc = 'terminals' },
-  { '<leader>fo', desc = 'tasks' },
+  { '<leader>fO', desc = 'terminals_manual' },
+  { '<leader>fo', desc = 'terminals_all' },
   { '<leader>Of', desc = 'find' },
 }
 
@@ -17,9 +17,70 @@ M.keymaps = function()
     vim.cmd [[ Telescope toggleterm_manager ]]
   end)
 
-  -- TODO: filter only overseer
   set_keymap('n', '<leader>fo', function()
-    vim.cmd [[ Telescope toggleterm_manager ]]
+    local pickers = require 'telescope.pickers'
+    local finders = require 'telescope.finders'
+    local conf = require('telescope.config').values
+    local actions = require 'telescope.actions'
+    local action_state = require 'telescope.actions.state'
+
+    local function get_terminal_buffers()
+      local bufs = vim.api.nvim_list_bufs()
+      local results = {}
+      for _, bufnr in ipairs(bufs) do
+        if vim.api.nvim_buf_is_loaded(bufnr) then
+          local name = vim.api.nvim_buf_get_name(bufnr)
+          if name:match '^term://' then
+            table.insert(results, {
+              bufnr = bufnr,
+              name = name ~= '' and name or '[No Name]',
+            })
+          end
+        end
+      end
+      return results
+    end
+
+    pickers
+      .new({}, {
+        prompt_title = 'Terminal Buffers',
+        finder = finders.new_table {
+          results = get_terminal_buffers(),
+          entry_maker = function(entry)
+            return {
+              value = entry.bufnr,
+              display = entry.name,
+              ordinal = entry.name,
+              bufnr = entry.bufnr,
+            }
+          end,
+        },
+        sorter = conf.generic_sorter {},
+        previewer = require('telescope.previewers').new_buffer_previewer {
+          define_preview = function(self, entry, status)
+            local bufnr = entry.bufnr or entry.value
+            if not bufnr or not vim.api.nvim_buf_is_valid(bufnr) then
+              return
+            end
+            local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
+            vim.api.nvim_buf_set_lines(self.state.bufnr, 0, -1, false, lines)
+            vim.api.nvim_buf_set_option(self.state.bufnr, 'filetype', 'terminal')
+          end,
+        },
+        attach_mappings = function(prompt_bufnr, map)
+          local select_term = function()
+            local entry = action_state.get_selected_entry()
+            actions.close(prompt_bufnr)
+            if entry and entry.bufnr then
+              vim.api.nvim_set_current_buf(entry.bufnr)
+            end
+          end
+          map('i', '<CR>', select_term)
+          map('n', '<CR>', select_term)
+          return true
+        end,
+      })
+      :find()
   end)
 end
 
